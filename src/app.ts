c2276fd3fb9e14fe11a08dbf4fe0265ffe7204ec
Douglas.Y <siyuan.yang2@uq.net.au>
2024-06-19 19:55:08 +1000

    fix: remove redundant routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express, { Application } from "express";
-import userRouter from "./routes/user.router";
 import authRouter from "./routes/auth.router";
 import videoRouter from "./routes/video.router";
 import cors from "cors";
@@ -11,7 +10,6 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/api/v1", userRouter);
 app.use("/api/v1", authRouter);
 app.use("/api/v1", videoRouter);
 
